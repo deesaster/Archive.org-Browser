d16418e759c3338c6384b37b7fe4bd839554f4c0
Denis Smirnov <denis@deesastudio.com>
2011-11-12 15:25:00 -0800

    Initial Commit

---
+++ BadAssery2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BadAssery2
+//
+//  Created by Denis Smirnov on 11-11-12.
+//  Copyright (c) 2011 Leetr.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
